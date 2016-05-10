4ff05e11b89d500fcc26f6b9010ef4e884f3df5d
Amrut Raut <amrut.raut@hmrcaspire.com>
2016-05-11 00:30:47 +0100

    update document parser

--- src/main/java/com/sainsbury/web/scraper/util/DocumentParser.java
+++ src/main/java/com/sainsbury/web/scraper/util/DocumentParser.java
@@ -3,10 +3,42 @@
  */
 package com.sainsbury.web.scraper.util;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * @author ARaut
  *
  */
 public class DocumentParser {
+	
+	public Document doc;
+	
+	public DocumentParser(Document doc){
+		this.doc =doc;
+	}
+	
+	public Element getElementByClass(Document doc, String className){
+		Element el = doc.getElementsByClass(className).first();
+		return el;
+	}
+	
+	public Element getElementByClass(Element el, String className){
+		return el.getElementsByClass(className).first();
+	}
+	
+	public Element getElementByTag(Element el, String tagName){
+		return el.getElementsByTag(tagName).first();
+	}
 
+	public Elements getElementsByTag(Element el, String tagName){
+		return el.getElementsByTag(tagName);
+	}
+
+	public Elements getElementsByTag(Document doc, String tagName){
+		return doc.getElementsByTag(tagName);
+	}
+
+	
 }
